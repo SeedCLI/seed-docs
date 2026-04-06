53ab5056c31ac25763276fe4d22051f70953548f
DeVoresyah Arest <de.voresyah@gmail.com>
2026-04-06 10:59:39 +0700

    feat: update docs for v1.1.1

--- worker/index.ts
+++ worker/index.ts
@@ -8,7 +8,7 @@ import { handleImageOptimization, DEFAULT_DEVICE_SIZES, DEFAULT_IMAGE_SIZES } fr
 import handler from "vinext/server/app-router-entry";
 
 interface Env {
-  ASSETS: Fetcher;
+  ASSETS: { fetch(input: RequestInfo | URL, init?: RequestInit): Promise<Response> };
   IMAGES: {
     input(stream: ReadableStream): {
       transform(options: Record<string, unknown>): {
